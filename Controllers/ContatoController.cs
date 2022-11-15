41b87b40f4b70494d07131eac784eb58a1c3ca13
limongi1234 <rntlemoslimongideaguiarmoraes@gmail.com>
2022-11-14 23:18:59 -0300

    Criação da página detalhes

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -65,5 +65,15 @@ namespace ProjetoMVC.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
     }
 }
