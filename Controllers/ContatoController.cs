1adbd4a916cf7c09d0a1804c02ebdba18394428c
limongi1234 <rntlemoslimongideaguiarmoraes@gmail.com>
2022-11-14 22:31:00 -0300

    Criação da página e funcionalidades de edição

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoMVC.Context;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Controllers
 {
@@ -25,6 +26,44 @@ namespace ProjetoMVC.Controllers
         public IActionResult Criar()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contato);
+        }
+
+         public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if(contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+        
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contato);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
